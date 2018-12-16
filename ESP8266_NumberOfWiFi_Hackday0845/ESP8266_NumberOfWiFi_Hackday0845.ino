80bc4502730c70325c4761f8c06e929036e74bf0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-12-16 11:48:44 +0900

    送信できていなかった

--- ESP8266_NumberOfWiFi_Hackday0845/ESP8266_NumberOfWiFi_Hackday0845.ino
+++ ESP8266_NumberOfWiFi_Hackday0845/ESP8266_NumberOfWiFi_Hackday0845.ino
@@ -52,6 +52,7 @@ void loop() {
           maxRSSI_SSID = ssid;
         }
         strip.setPixelColor(i, strip.Color(rssi % 255, 32, 0));
+        strip.show();
       }
       delay(500);
       return;
